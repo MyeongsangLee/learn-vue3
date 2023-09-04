4bc407bf4906077c53d55ec616a0f9fe5b27731b
Lee Myeongsang <mslee0052@gmail.com>
2023-09-04 16:07:58 +0900

    commit

---
+++ src/lectures/7/App_reactive.vue
@@ -0,0 +1,29 @@
+<template>
+	<button v-on:click="increment">Click {{ state.count }}</button>
+	<button v-on:click="increment">Deep Click {{ state.deep.count }}</button>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		const state = reactive({
+			count: 0,
+			deep: {
+				count: 0,
+			},
+		});
+		const increment = () => {
+			state.count++;
+			state.deep.count++;
+		};
+		return {
+			state,
+			increment,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
